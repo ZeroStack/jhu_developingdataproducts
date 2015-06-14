52d175d70569571526e04f05ef0620e0dbd1ca6a
ZeroStack <sahilmp8@gmail.com>
2015-06-14 16:45:50 +0100

    Added Shiny hosted app link

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ library("dplyr")
 library("stringr")
 
 # Sets working directory to source of file
-setwd("/Users/Sahil/Courses/Coursera/Developing Data Products/Course Project/app")
+#setwd("/Users/Sahil/Courses/Coursera/Developing Data Products/Course Project/appdp")
 
 # Load data
 forbes_data <- fread("data/ObservationData_mthyecc.csv")
@@ -18,13 +18,19 @@ forbes_data <- fread("data/ObservationData_mthyecc.csv")
 adjustData <- function(data) {
         # Names of data
         data.names <- names(data)
+        
         # Set names of data
-        setnames(data, data.names, tolower(str_replace_all(data.names, "([ |$|,])", ".")))
+        setnames(data, data.names, tolower(str_replace_all(data.names, "([ |$|,])", "")))
+        
+        #select relevant data
+        data <- select(data, age, networthb, time)
+        
         data$age <- as.numeric(data$age)
-        data$net.worth...b <- as.numeric(data$net.worth...b)
-        data$v8 <- NULL
+        data$networthb <- as.numeric(data$networthb)
+
         data$age[data$age == 0] <- NA
-        data <- arrange(data, desc(net.worth...b)) 
+        data <- arrange(data, desc(networthb))
+        data <- na.omit(data)
         # Return data
         return(data)
         
@@ -37,8 +43,9 @@ shinyServer(function(input, output, session) {
         
         
         output$distPlot <- renderPlot({
+                
                 forbes.data <- filter(forbes.data, time == input$year)
-                plot <- ggplot(forbes.data, aes(x = age, y = net.worth...b)) + geom_bar(stat = "identity") + xlab("Age in Years") + ylab("Bn Net Worth (US$)")
+                plot <- ggplot(forbes.data, aes(x = age, y = networthb)) + geom_bar(stat = "identity") + xlab("Age in Years") + ylab("Bn Net Worth (US$)")
                 plot
 #         
         })
